48cb003508ee3c3b1a1e656fd940e626f6d97c03
AagamShah <shahaagam2314@gmail.com>
2024-06-28 20:38:07 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div",{ id:"child" },[
-        React.createElement("h1",{ color:"blue" },"I am an h1 tag"),
-        React.createElement("h2",{ color:"red" },"I am an h2 tag")
-    ]),
-    React.createElement("div",{ id:"child2" },[
-        React.createElement("h1",{ color:"green" },"I am an h1 tag"),
-        React.createElement("h2",{ color:"grey" },"I am an h2 tag")
-    ]),
-]);
+//React Elements in JSX which is compiled by BABEL
+//JSX parses injected data before running it within the site in order to prevent XSS attacks
+//Functional Component
+const name = <span>Aagam</span>;
+const Title = () => <h1 className='head'>{name} React is awesome</h1>
 
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const Footer = () => <p className='footer'>This is footer</p>
+
+
+//Component Composition: One component in the other
+const HeadingComponent = () => (
+    <div id='container'>
+        <Title />
+        <br />
+        {name}
+        <br />
+        <h1 className='heading'>Hello Aagam</h1>
+        <Footer />
+    </div>
+);
+
+//console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ->Every dependencies will have their own package.json
 
 ->Node modules: Conatain all the dependencies code and their dependencies
+
+->Babel: Converts JSX to ES6
